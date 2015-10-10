e751cd0b1653b911474ea31338da0a9db787b121
bruno <bruno@10.0.2.15>
2015-10-10 00:20:43 -0500

    ok

---
+++ src/domain/Pedido.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package domain;
+
+/**
+ *
+ * @author bruno
+ */
+import java.util.*;
+public class Pedido {
+    private List<ItemPedido> item;
+    private Usuario usuario;
+    private double precio_total;
+    private Date fecha_pedido;
+    private int estado_pedido;
+    private TipoPago tipo_pago;
+
+    public int getEstado_pedido() {
+        return estado_pedido;
+    }
+
+    public void setEstado_pedido(int estado_pedido) {
+        this.estado_pedido = estado_pedido;
+    }
+
+    public Date getFecha_pedido() {
+        return fecha_pedido;
+    }
+
+    public void setFecha_pedido(Date fecha_pedido) {
+        this.fecha_pedido = fecha_pedido;
+    }
+
+    public List<ItemPedido> getItem() {
+        return item;
+    }
+
+    public void setItem(List<ItemPedido> item) {
+        this.item = item;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public double getPrecio_total() {
+        return precio_total;
+    }
+
+    public void setPrecio_total(double precio_total) {
+        this.precio_total = precio_total;
+    }
+    
+    
+}
